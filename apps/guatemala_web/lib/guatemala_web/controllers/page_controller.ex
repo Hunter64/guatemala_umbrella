b705d5ba05ba7c48dde1274df3e57b82c52bd80b
Hector Lopez <hector.lopez@DSA-HELOPEZ>
2022-06-14 10:21:03 -0500

    Page controller index

--- apps/guatemala_web/lib/guatemala_web/controllers/page_controller.ex
+++ apps/guatemala_web/lib/guatemala_web/controllers/page_controller.ex
@@ -1,7 +1,10 @@
 defmodule GuatemalaWeb.PageController do
   use GuatemalaWeb, :controller
+  alias Phoenix.LiveView
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    #render(conn, "index.html")
+    conn
+      |> LiveView.Controller.live_render(GuatemalaWeb.PageLiveView, session: %{})
   end
 end
--- apps/guatemala_web/lib/guatemala_web/views/page_view.ex
+++ apps/guatemala_web/lib/guatemala_web/views/page_view.ex
@@ -1,3 +1,20 @@
 defmodule GuatemalaWeb.PageView do
   use GuatemalaWeb, :view
 end
+
+defmodule GuatemalaWeb.PageLiveView do
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    Phoenix.View.render(GuatemalaWeb.PageView, "index.html", assigns)
+  end
+
+  def mount(_params, session, socket) do
+    session = (for {key, val} <- session, into: %{}, do: {String.to_atom(key), val})
+
+      {:ok, assign(socket,
+      session: session
+      )}
+  end
+
+end
