d3b7442699f6dc0a7852e82471f05bb0550d1385
Hector Lopez <hector.lopez@DSA-HELOPEZ>
2022-07-19 17:06:21 -0500

    add ecto util errors control

---
+++ apps/guatemala/lib/guatemala/common/ecto_util.ex
@@ -0,0 +1,17 @@
+defmodule Guatemala.EctoUtil do
+  @moduledoc """
+  Módulo para convertir lista de errores de ecto en un string con formato
+  """
+  def get_errors(changeset) do
+    get_errors(changeset.errors, "")
+  end
+
+  defp get_errors([], error) do
+    error
+  end
+
+  defp get_errors([{atom, {message, _array}} | tail], error) do
+    get_errors(tail, error <> "#{atom}: #{message}<br>")
+  end
+
+end
