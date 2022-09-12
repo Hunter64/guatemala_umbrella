f01c3f516fbb59728070119003143a8c0b160e5d
Hector Lopez <hector.lopez@DSA-HELOPEZ>
2022-09-11 21:26:26 -0500

    working saving customer

--- apps/guatemala_web/lib/guatemala_web/components/customers/form_new_edit_customers_component.ex
+++ apps/guatemala_web/lib/guatemala_web/components/customers/form_new_edit_customers_component.ex
@@ -5,6 +5,9 @@ defmodule GuatemalaWeb.FormNewEditCustomersComponent do
   use Phoenix.LiveComponent
   use Phoenix.HTML
 
+  alias Guatemala.GenericFunctions, as: Generic
+  alias Guatemala.Customers, as: Customers
+
   def mount(socket) do
     {:ok, socket}
   end
@@ -25,87 +28,154 @@ defmodule GuatemalaWeb.FormNewEditCustomersComponent do
 
           <div class="h-hoch-80 px-4 w-full relative mt-2">
 
-            <div class="border-solid border-2 border-amber-700 px-4 py-1 rounded">
-              <div>
-                <label class="text-base font-bold text-amber-700 dark:text-white">Rellene los campos necesarios</label>
-              </div>
+            <form id="form_customer" phx-submit="save_customer" phx-target="#form_new_edit_customer">
+              <div class="border-solid border-2 border-amber-700 px-4 py-1 rounded">
+                <div>
+                  <label class="text-base font-bold text-amber-700 dark:text-white">Rellene los campos necesarios</label>
+                </div>
 
-              <div class="w-full inline-block">
+                <div class="w-full inline-block">
 
-                <div class="w-full inline-flex">
+                  <div class="w-full inline-flex">
+                    <div class="w-15/100 py-2">
+                      <label class="text-base font-normal text-amber-700 dark:text-white">Primer Nombre:</label>
+                    </div>
 
+                    <div class="w-30/100 py-2">
+                      <input type="text" name="first_name" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="first_name" phx-target="#form_customers" id="input_first_name" placeholder="Primer Nombre" value="">
+                    </div>
 
-                  <div class="w-15/100 py-2">
-                    <label class="text-base font-normal text-amber-700 dark:text-white">Primer Nombre:</label>
+                    <div class="w-10/100 py-2">
+                    </div>
+
+                    <div class="w-15/100 py-2">
+                      <label class="text-base font-normal text-amber-700 dark:text-white">Segundo Nombre:</label>
+                    </div>
+
+                    <div class="w-30/100 py-2">
+                      <input type="text" name="second_name" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="second_name" phx-target="#form_customers" id="input_second_name" placeholder="Segundo Nombre" value="">
+                    </div>
                   </div>
 
-                  <div class="w-30/100 py-2">
-                    <input type="text" name="clave" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="clave" phx-target="#form_customers" id="input_clave" placeholder="Primer Nombre" value="">
+                  <div class="w-full inline-flex">
+                    <div class="w-15/100 py-2">
+                      <label class="text-base font-normal text-amber-700 dark:text-white">Primer Apellido:</label>
+                    </div>
+
+                    <div class="w-30/100 py-2">
+                      <input type="text" name="first_surname" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="first_surname" phx-target="#form_customers" id="input_first_surname" placeholder="Primer Apellido" value="">
+                    </div>
+
+                    <div class="w-10/100 py-2">
+                    </div>
+
+                    <div class="w-15/100 py-2">
+                      <label class="text-base font-normal text-amber-700 dark:text-white">Segundo Apellido:</label>
+                    </div>
+
+                    <div class="w-30/100 py-2">
+                      <input type="text" name="second_surname" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="second_surname" phx-target="#form_customers" id="input_second_surname" placeholder="Segundo Apellido" value="">
+                    </div>
                   </div>
 
-                  <div class="w-10/100 py-2">
+                  <div class="w-full inline-flex">
+                    <div class="w-15/100 py-2">
+                      <label class="text-base font-normal text-amber-700 dark:text-white">Teléfono:</label>
+                    </div>
+
+                    <div class="w-30/100 py-2">
+                      <input type="text" name="phone" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="phone" phx-target="#form_customers" id="input_phone" placeholder="Teléfono" value="">
+                    </div>
+
+                    <div class="w-10/100 py-2">
+                    </div>
+
+                    <div class="w-15/100 py-2">
+                      <label class="text-base font-normal text-amber-700 dark:text-white">Email:</label>
+                    </div>
+
+                    <div class="w-30/100 py-2">
+                      <input type="text" name="email" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="email" phx-target="#form_customers" id="input_email" placeholder="Email" value="">
+                    </div>
                   </div>
 
-                  <div class="w-15/100 py-2">
-                    <label class="text-base font-normal text-amber-700 dark:text-white">Segundo Nombre:</label>
-                  </div>
+                </div>
 
-                  <div class="w-30/100 py-2">
-                    <input type="text" name="large_name" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="large_name" phx-target="#form_customers" id="input_large_name" placeholder="Segundo Nombre" value="">
-                  </div>
+                <div class="py-2">
+                  <label class="text-base font-normal text-amber-700 dark:text-white block">Comentarios:</label>
+                  <input type="text" name="comments" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="comments" phx-target="#form_customers" id="input_comments" placeholder="Comentarios" value="">
                 </div>
 
                 <div class="w-full inline-flex">
-                  <div class="w-15/100 py-2">
-                    <label class="text-base font-normal text-amber-700 dark:text-white">Primer Apellido:</label>
-                  </div>
-
-                  <div class="w-30/100 py-2">
-                    <input type="text" name="clave" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="clave" phx-target="#form_customers" id="input_clave" placeholder="Primer Apellido" value="">
-                  </div>
-
-                  <div class="w-10/100 py-2">
-                  </div>
-
-                  <div class="w-15/100 py-2">
-                    <label class="text-base font-normal text-amber-700 dark:text-white">Segundo Apellido:</label>
-                  </div>
-
-                  <div class="w-30/100 py-2">
-                    <input type="text" name="large_name" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="large_name" phx-target="#form_customers" id="input_large_name" placeholder="Segundo Apellido" value="">
+                  <div class="w-75/100 py-2"></div>
+                  <div class="w-25/100 py-2">
+                    <button type="submit" form="form_customer" class="shadow py-2 bg-green-500 dark:bg-green-400 hover:bg-green-400 dark:hover:bg-green-300 text-white items-center inline-flex font-bold rounded text-sm w-full">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-4 w-4 mr-2 ml-auto">
+                        <path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z" />
+                        <path fill-rule="evenodd" d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zM12 10.5a.75.75 0 01.75.75v4.94l1.72-1.72a.75.75 0 111.06 1.06l-3 3a.75.75 0 01-1.06 0l-3-3a.75.75 0 111.06-1.06l1.72 1.72v-4.94a.75.75 0 01.75-.75z" clip-rule="evenodd" />
+                      </svg>
+                      <label class="cursor-pointer mr-auto text-white">Guardar</label>
+                    </button>
                   </div>
                 </div>
-
               </div>
+            </form>
 
-
-
-
-              <div class="py-2">
-                <label class="text-base font-normal text-amber-700 dark:text-white">Razón Social:</label>
-                <input type="text" name="social_reason" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="social_reason" phx-target="#form_customers" id="input_social_reason" placeholder="Razón Social" value="">
-              </div>
-
-              <div class="py-2">
-                <label class="text-base font-normal text-amber-700 dark:text-white block">RFC:</label>
-                <input type="text" name="rfc" maxlength="128" class="shadow w-full px-2 py-1 border-amber-100 focus:border-amber-500 text-sm appearance-none block text-gray-700 border rounded leading-tight focus:outline-none focus:bg-white" phx-value-name="rfc" phx-target="#form_customers" id="input_rfc" placeholder="RFC" value="">
-              </div>
-
-              <div class="py-2">
-                <button phx-click="search_customers" phx-target="#form_customers" class="shadow py-2 bg-green-500 dark:bg-green-400 hover:bg-green-400 dark:hover:bg-green-300 text-white items-center inline-flex font-bold rounded text-sm w-full">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4 mr-2 ml-auto">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                  </svg>
-                  <label class="cursor-pointer mr-auto text-white">Buscar</label>
-                </button>
-              </div>
-
-            </div>
           </div>
 
 
       </div>
     """
+  end
+
+  def handle_event("save_customer", params, socket) do
+    params |> IO.inspect(label: " ---------------------> OPARAMS ")
+    customer_pre =%{
+      active: true,
+      catalog_id: 1,
+      comments: params["comments"] |> validate_possible_nil(false),
+      creator_user_id: 1,
+      credit_type_id: 1,
+      first_name: params["first_name"] |> String.trim() |> Generic.titelize_sentence,
+      first_surname: params["first_surname"] |> String.trim() |> Generic.titelize_sentence,
+      large_name: params |> get_large_name(),
+      moral_person: false,
+      rfc: "XAXX010101000",
+      second_name: params["second_name"] |> String.trim() |> Generic.titelize_sentence,
+      second_surname: params["second_surname"] |> String.trim() |> Generic.titelize_sentence,
+      social_reason: "XXX"
+    }
+
+    customer_pre
+      |> Customers.create_customer()
+      |> IO.inspect(label: " -------------------> NEW CUSTOMER ?")
+
+    {:noreply, assign(
+      socket,
+      x: "adsf"
+      )
+    }
+  end
+
+  def get_large_name(params) do
+    (params["first_name"] |> String.trim() |> Generic.titelize_sentence)
+    <> " " <>
+    (params["second_name"] |> validate_possible_nil(true))
+    <>
+    (params["first_surname"] |> String.trim() |> Generic.titelize_sentence)
+    <> " " <>
+    (params["second_surname"] |> validate_possible_nil(false))
+  end
+
+  def validate_possible_nil(data, last_space) do
+    data
+      |> Generic.valid_string_data()
+      |> case do
+        true ->
+          (data |> String.trim() |> Generic.titelize_sentence) <> (if last_space, do: " ", else: "")
+        false ->
+          ""
+      end
   end
 
 end
