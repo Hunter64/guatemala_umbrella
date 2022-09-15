3ed2357dafc417cd822cddd6042b3a1adee579d4
Hector Lopez <hector.lopez@DSA-HELOPEZ>
2022-09-15 11:13:21 -0500

    fix migration file phones

--- apps/guatemala/priv/repo/migrations/20220915155113_create_phones.exs
+++ apps/guatemala/priv/repo/migrations/20220915155113_create_phones.exs
@@ -8,7 +8,7 @@ defmodule Guatemala.Repo.Migrations.CreatePhones do
       add :country_code, :string, size: 10, null: false
       add :lada_code, :string, size: 10, null: false
       add :number, :string, size: 10, null: false
-      add :extension, :string size: 10
+      add :extension, :string, size: 10
       add :active, :boolean, default: false, null: false
       add :creator_user_id, :integer, null: false
       add :modifier_user_id, :integer
