15a5d5c3301b92382a32f1789c00931c32a49167
Hector Lopez <hector.lopez@DSA-HELOPEZ>
2022-09-27 14:26:51 -0500

    building customers list

--- apps/guatemala_web/lib/guatemala_web/components/customers/list_customers_component.ex
+++ apps/guatemala_web/lib/guatemala_web/components/customers/list_customers_component.ex
@@ -39,17 +39,23 @@ defmodule GuatemalaWeb.ListCustomersComponent do
                     <table class="table-auto w-full">
                         <thead class="text-xs font-bold uppercase text-white bg-amber-700 dark:bg-slate-400">
                             <tr>
-                                <th class="p-2 whitespace-nowrap">
+                                <th class="p-2 whitespace-nowrap w-10/100">
                                     <div class="text-left">Clave</div>
                                 </th>
-                                <th class="p-2 whitespace-nowrap">
+                                <th class="p-2 whitespace-nowrap w-40/100">
                                     <div class="text-left">Nombre Corto</div>
                                 </th>
-                                <th class="p-2 whitespace-nowrap">
+                                <th class="p-2 whitespace-nowrap w-15/100">
                                     <div class="text-left">Teléfono</div>
                                 </th>
-                                <th class="p-2 whitespace-nowrap">
+                                <th class="p-2 whitespace-nowrap w-15/100">
                                     <div class="text-center">Email</div>
+                                </th>
+                                <th class="p-2 whitespace-nowrap w-10/100">
+                                    <div class="text-center">Estatus</div>
+                                </th>
+                                <th class="p-2 whitespace-nowrap w-10/100">
+                                    <div class="text-center">Editar</div>
                                 </th>
                             </tr>
                         </thead>
@@ -57,22 +63,43 @@ defmodule GuatemalaWeb.ListCustomersComponent do
 
                           <%= for item <- @all_customers do %>
 
-                            <tr class="bg-amber-100 dark:bg-slate-200">
-                              <td class="p-2 whitespace-nowrap">
+                            <tr class="bg-amber-100 dark:bg-slate-200 w-full">
+                              <td class="p-2 whitespace-nowrap w-10/100">
                                   <div class="text-left"><%= item.id %></div>
                               </td>
-                              <td class="p-2 whitespace-nowrap">
+                              <td class="p-2 whitespace-nowrap w-40/100">
                                   <div class="flex items-center">
-
                                       <div class="w-8 h-8 flex-shrink-0 mr-2 sm:mr-3"><img class="rounded-full" src="https://www.svgrepo.com/show/362184/user-male.svg" width="30" height="30" srcset="https://www.svgrepo.com/show/362184/user-male.svg 4x" alt="Customer"></div>
                                       <div class="font-medium text-gray-800"><%= item.first_name <> " " <> item.first_surname %></div>
                                   </div>
                               </td>
-                              <td class="p-2 whitespace-nowrap">
+                              <td class="p-2 whitespace-nowrap w-15/100">
                                   <div class="text-left">5578457845</div>
                               </td>
-                              <td class="p-2 whitespace-nowrap">
+                              <td class="p-2 whitespace-nowrap w-15/100">
                                   <div class="text-left font-medium">test@gmail.com</div>
+                              </td>
+                              <td class="p-2 whitespace-nowrap w-10/100">
+                                <%= if item.active do %>
+                                  <div class="font-medium">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                                      <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" class="text-green-500" />
+                                    </svg>
+                                  </div>
+                                <% else %>
+                                  <div class="font-medium">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                                      <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clip-rule="evenodd" class="text-red-500" />
+                                    </svg>
+                                  </div>
+                                <% end %>
+                              </td>
+                              <td class="p-2 whitespace-nowrap w-10/100">
+                                  <div class="font-medium">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 align-center">
+                                      <path d="M18.75 12.75h1.5a.75.75 0 000-1.5h-1.5a.75.75 0 000 1.5zM12 6a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 6zM12 18a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 18zM3.75 6.75h1.5a.75.75 0 100-1.5h-1.5a.75.75 0 000 1.5zM5.25 18.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5zM3 12a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 013 12zM9 3.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zM12.75 12a2.25 2.25 0 114.5 0 2.25 2.25 0 01-4.5 0zM9 15.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
+                                    </svg>
+                                  </div>
                               </td>
 
                           </tr>
@@ -80,7 +107,11 @@ defmodule GuatemalaWeb.ListCustomersComponent do
                           <% end %>
 
                         </tbody>
+
                     </table>
+                    <div class="w-full text-white mt-2">
+                      Total de Registros:  <b><%= @all_customers |> Enum.count %></b>
+                    </div>
                 </div>
             </div>
         </div>
