039cf06415981eae2b5bb98148763982bfcb5f4c
Hunter <lopezhector233@gmail.com>
2022-12-17 17:56:58 -0600

    add btn to come back to list customers from edit

--- apps/guatemala_web/lib/guatemala_web/components/customers/form_edit_customers_component.ex
+++ apps/guatemala_web/lib/guatemala_web/components/customers/form_edit_customers_component.ex
@@ -219,12 +219,32 @@ defmodule GuatemalaWeb.FormEditCustomersComponent do
 
           </div>
         </form>
+
+          <div class="w-full inline-flex px-4 py-2">
+            <div class="w-75/100 py-2"></div>
+            <div class="w-25/100 py-2">
+              <button phx-click="back_to_list_customer" phx-target="#form_edit_customer" class="shadow py-2 bg-green-500 dark:bg-green-400 hover:bg-green-400 dark:hover:bg-green-300 text-white items-center inline-flex font-bold rounded text-sm w-full">
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-4 w-4 mr-2 ml-auto">
+                  <path fill-rule="evenodd" d="M2.625 6.75a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0A.75.75 0 018.25 6h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75zM2.625 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 12a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12A.75.75 0 017.5 12zm-4.875 5.25a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+                </svg>
+                <label class="cursor-pointer mr-auto text-white">Volver al listado</label>
+
+              </button>
+            </div>
+          </div>
+
       </div>
 
       <%= if @description_alert, do: live_component(GuatemalaWeb.NotificationComponent, id: @notification_type, header: @header, description_alert: @description_alert, show: true, notification_type: @notification_type, change: @change) %>
 
     </div>
     """
+  end
+
+  def handle_event("back_to_list_customer", _params, socket) do
+    send_update(GuatemalaWeb.ListCustomersComponent, id: "list_customers", filters: socket.assigns.filters_to_return_from_edit)
+    {:noreply, socket}
   end
 
   def handle_event("edit_phones", _params, socket) do
